673ad4fc851da5e8c825f20c4c29d85bcf2d08e0
maruyan <7115497+hmaruyama@users.noreply.github.com>
2022-03-23 12:11:17 +0900

    環境変数をを使用するように変更

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -89,17 +89,6 @@
  * @endcode
  */
 
- $databases['default']['default'] = [
-  'database' => getenv('DB_NAME'),
-  'username' => getenv('DB_USER'),
-  'password' => getenv('DB_PASS'),
-  'host' => getenv('DB_HOST'),
-  'port' => '3306',
-  'driver' => 'mysql',
-  'prefix' => '',
-];
-
-
 /**
  * Customizing database settings.
  *
@@ -779,12 +768,13 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
 $databases['default']['default'] = array (
-  'database' => 'drupal9',
-  'username' => 'drupal9',
-  'password' => 'drupal9',
+  'database' => getenv('DB_NAME'),
+  'username' => getenv('DB_USER'),
+  'password' => getenv('DB_PASS'),
   'prefix' => '',
-  'host' => 'database',
+  'host' => getenv('DB_HOST'),
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
